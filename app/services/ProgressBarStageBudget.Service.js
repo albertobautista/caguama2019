26d965bc8a9bf637617316708783afe85eff2df1
HiramGamiz <33489236+HiramGamiz@users.noreply.github.com>
2019-09-06 13:08:32 -0500

    bar

--- app/services/ProgressBarStageBudget.Service.js
+++ app/services/ProgressBarStageBudget.Service.js
@@ -27,8 +27,10 @@
     return service;
 
     function get(filter) {
+      console.log('prrroorororororororororo')
+      console.log(filter)
       if (baseUrl != null) {
-        config.url = baseUrl + '/services/apexrest/t2go1/Stage_Budge /?' + 'idStage=' + filter.idStage + '&codeAgent=' + filter.idStage
+        config.url = baseUrl + '/services/apexrest/t2go1/Stage_Budge/?' + 'idStage=' + filter.idStage + '&codeAgent=' + filter.idStage
         contadorurl = 0
       } else {
         if (contadorurl == 0) {
